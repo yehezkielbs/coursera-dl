9f3c70423e841023f9b41aaeba139979da55c097
Dirk Gorissen <dgorissen@gmail.com>
2013-02-03 15:26:33 +0000

    bumped version number

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup
 
 setup(name="coursera-dl",
-            version="1.2",
+            version="1.2.1",
             description="Download coursera.org class videos and resources",
             long_description=open("README.md").read(),
             author="Dirk Gorissen",
